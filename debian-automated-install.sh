f1292a3accff54c2817212eee0916ec5ac5d1de3
L3vi47h4N <l3viath4n@gmail.com>
2015-11-11 03:42:02 -0500

    Create debian-automated-install.sh
    Ground-up rerwrite of the Debian auto-install script.

---
+++ debian-automated-install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+#Designed for Debian Jessie.
+#This script installs dependencies, downloads OpenCog from Github, compiles and installs it, and leaves the build environment.
+#Script also allows for building with Moses.
+#Author: Noah Bliss
+#Last editor: 
+#Last updated on: 11/11/2015
