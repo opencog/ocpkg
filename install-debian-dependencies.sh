58f9e9385f05a05be3745580491a23e29aa474d1
L3vi47h4N <l3viath4n@gmail.com>
2015-11-11 02:26:14 -0500

    Update install-debian-dependencies.sh
    Made inline text prettier.

--- install-debian-dependencies.sh
+++ install-debian-dependencies.sh
@@ -136,6 +136,7 @@ fi
 install_dependencies
 install_python_packages
 
+printf '\n \n'
 echo "Dependencies installed, we can clone OpenCog, Atomspace, etc to the current directory if you like."
 read -p "Download Opencog source to current path? (y/n) " gitclone
 if [ "$gitclone" == "y" ] || [ "$gitclone" == "Y" ]
